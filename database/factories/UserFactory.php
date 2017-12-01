00c04c1b0f5934f37d2204a25e0921d1a6a26e58
ChenJing <654807719@qq.com>
2017-12-01 22:39:47 +0800

    创建数据库迁移

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,22 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'avatar' => $faker->imageUrl(256, 256),
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Discussion::class, function (Faker $faker) {
+	static $user_ids;
+	if(!$user_ids)
+		//此处转化为1维数组，视频中的lists('id')方法不存在
+    	$user_ids = collect(\App\User::all('id')->toArray())->flatten()->toArray();
+
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => $faker->randomElement($user_ids),
+        'last_user_id' => $faker->randomElement($user_ids),
     ];
 });
