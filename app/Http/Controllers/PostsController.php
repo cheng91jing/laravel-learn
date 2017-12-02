c8d579ca65db6021e381e75c45aee670df4a0c46
ChenJing <654807719@qq.com>
2017-12-02 08:48:16 +0800

    home page

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Discussion;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $discussions = Discussion::all();
+    	return view('forum.index', [
+    	    'discussions' => $discussions
+        ]);
+    }
+}
---
+++ resources/views/forum/index.blade.php
@@ -0,0 +1,28 @@
+@extends('app')
+
+@section('content')
+    <div class="jumbotron">
+        <div class="container">
+            <h2>欢迎来到Chen的 <strong><em>Laravel</em></strong> 学习空间! <a class="btn btn-primary btn-lg pull-right" href="#" role="button">发布新的帖子</a></h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9" role="main">
+                @foreach($discussions as $discussion)
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img src="{{ $discussion->user->avatar }}" alt="64x64" class="media-object img-circle" style="width: 64px; height: 64px;">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">{{ $discussion->title }}</h4>
+                            {{ $discussion->user->name }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
