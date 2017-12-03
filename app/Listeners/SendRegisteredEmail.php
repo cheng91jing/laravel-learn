e5e1e984ea4a1ee411b2c5817585d758d4161c16
ChenJing <654807719@qq.com>
2017-12-03 10:33:52 +0800

    优化注册邮件发送格式

--- app/Listeners/SendRegisteredEmail.php
+++ app/Listeners/SendRegisteredEmail.php
@@ -3,6 +3,8 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Mail\Markdown;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Mail;
@@ -27,12 +29,22 @@ class SendRegisteredEmail
      */
     public function handle(Registered $event)
     {
-        $view = 'emails.register';
+//        $view = 'emails.register';
         $data['user'] = $event->user;
 //        $from = 'aufree@yousails.com';
 //        $name = 'Aufree';
         $to = $event->user->email;
-        $subject = "感谢注册 Laravel Learn！请确认你的邮箱。";
+        $subject = "感谢注册 LaravelLearn！请确认你的邮箱。";
+        $message = (new MailMessage())
+            ->line('感谢您在 '. config('app.name') .' 网站进行注册！')
+            ->line('请点击下面的链接完成注册：')
+            ->action('验证邮箱', route('confirm_email', ['confirm_code' => $event->user->email_confirm_code]))
+            ->line('如果这不是您本人的操作，请忽略此邮件。');
+        $markdown = app()->make(Markdown::class);
+        $view = [
+            'html' => $markdown->render($message->markdown, $message->data()),
+            'text' => $markdown->renderText($message->markdown, $message->data()),
+        ];
 
         Mail::send($view, $data, function ($message) use ($to, $subject) {
             $message->to($to)->subject($subject);
