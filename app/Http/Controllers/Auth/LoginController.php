28cffe3e1423a33b7a64a06d128af65b3fe3080c
ChenJing <654807719@qq.com>
2017-12-04 22:01:45 +0800

    login redirect to previous url

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,29 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        $this->setPrevious();
+        return view('auth.login');
+    }
+
+    protected function redirectTo()
+    {
+        if(session()->has('login_previous'))
+            return session()->pull('login_previous');
+        return $this->redirectTo;
+    }
+
+    protected function setPrevious()
+    {
+        $root = url()->getRequest()->root();
+        $previous = url()->previous();
+        $relatively = Str::after($previous, $root);
+        if($relatively != $previous && route('login') != $previous)
+            session(['login_previous' => $relatively]);
+        else
+            session()->forget('login_previous');
     }
 }
