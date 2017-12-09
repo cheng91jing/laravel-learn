40ad630fefcbd772896c6108484d4ab6f3ddc38b
ChenJing <654807719@qq.com>
2017-12-09 18:08:10 +0800

    role model 关系

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    public function permissions()
+    {
+        return  $this->belongsToMany(
+            Permission::class,          //目标模型
+            'permission_role',           //连接表表
+            'role_id',           //此关联模型在连接表中的键名
+            'permission_id',     //另一个模型在连接表中的键名
+            'id',                   //此模型的主键
+            'id'                    //另一个模型的主键
+        );
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
